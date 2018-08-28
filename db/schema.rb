155cb64468564a41a1cc5b73c0d13d4377263728
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-28 13:55:47 -0500

    elimina migracio de admin y se pone en la creacion de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_031321) do
+ActiveRecord::Schema.define(version: 2018_08_27_190605) do
 
   create_table "activities", force: :cascade do |t|
     t.integer "user_id"
@@ -37,12 +37,12 @@ ActiveRecord::Schema.define(version: 2018_08_28_031321) do
     t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.boolean "admin", default: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
